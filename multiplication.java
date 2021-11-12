b8ebf2524d92cf83432c9395183becbb9c73fac0
root <root@ip-172-31-6-169.ap-south-1.compute.internal>
2021-11-12 14:05:56 +0000

    updated java code

---
+++ multiplication.java
@@ -0,0 +1,6 @@
+import java.util.Scanner; class MultiplicationProject { public static void main(String args[]) { int n, c; 
+System.out.println("Enter an integer to print it's multiplication table"); Scanner in = new Scanner(System.in); n 
+= in.nextInt(); System.out.println("Multiplication table of "+n+" is :-"); for ( c = 1 ; c <= 10 ; c++ ) 
+System.out.println(n+"*"+c+" = "+(n*c));
+}
+}
